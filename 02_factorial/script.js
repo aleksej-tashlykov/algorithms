0a233a31cf9c3c853689fc4ca74440176509a884
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-08-22 19:26:29 +1000

    Исходный код

---
+++ 02_factorial/script.js
@@ -0,0 +1,20 @@
+function factorialRecursive(n) {
+	if (n === 0 || n === 1) {
+		return 1;
+	} else {
+		return n * factorialRecursive(n - 1);
+	}
+}
+
+function factorialIterative(n) {
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+
+	let result = 1;
+	for (let i = 2; i <= n; i++) {
+		result *= i;
+	}
+
+	return result;
+}
