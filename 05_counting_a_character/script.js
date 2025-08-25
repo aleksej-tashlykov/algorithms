d4a63a87a1480a9cacdd9e2b7287162d33d1b42f
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-08-25 19:44:23 +1000

    Исходный код без учета регистра

--- 05_counting_a_character/script.js
+++ 05_counting_a_character/script.js
@@ -10,7 +10,7 @@ function countingCharacter(str, char) {
 	let count = 0;
 
 	for (let i = 0; i < str.length; i++) {
-		if (str[i].toLowerCase() === char.toLowerCase()) {
+		if (str[i] === char) {
 			count++;
 		}
 	}
