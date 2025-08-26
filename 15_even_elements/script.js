b2cfe09276d5ddc560d46599c314f07a8b207456
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-08-26 19:52:53 +1000

    Исходный код

---
+++ 15_even_elements/script.js
@@ -0,0 +1,12 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+if (array.length === 0) {
+	return;
+}
+
+for (let i = 0; i < array.length; i++) {
+	if (array[i] % 2 === 0) {
+		array[i] = 0;
+	}
+}
+
