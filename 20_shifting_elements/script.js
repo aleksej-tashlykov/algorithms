89e56fa8bd5ef649f332dee6d4e0c029f7b16f7a
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-08-27 21:29:19 +1000

    Исправленный исходный код

--- 20_shifting_elements/script.js
+++ 20_shifting_elements/script.js
@@ -5,12 +5,23 @@ if (arr.length === 0) {
 	return;
 }
 
-for (let i = arr.length - 1; i >= 0; i--) {
-	if (i + offset < arr.length) {
+const length = arr.length;
+
+if (offset > 0) {
+	for (let i = length - 1 - offset; i >= 0; i--) {
 		arr[i + offset] = arr[i];
 	}
-}
 
-for (let i = 0; i < offset; i++) {
-	arr[i] = 0;
+	for (let i = 0; i < offset; i++) {
+		arr[i] = 0;
+	}
+} else if (offset < 0) {
+	const shift = -offset;
+	for (let i = 0; i < length - shift; i++) {
+		arr[i] = arr[i + shift];
+	}
+
+	for (let i = length - shift; i < length; i++) {
+		arr[i] = 0;
+	}
 }
