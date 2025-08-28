e6bb4078cd47f97ce5234e8289fb0942fc34c61f
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-08-28 21:14:34 +1000

    Исходный код

---
+++ 21_pairwise_exchange_elements/script.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+if (arr.length === 0) {
+	return;
+}
+
+for (let i = 0; i < arr.length; i += 2) {
+	if (i + 1 < arr.length) {
+		let temp = arr[i];
+		arr[i] = arr[i + 1];
+		arr[i + 1] = temp;
+	}
+}
