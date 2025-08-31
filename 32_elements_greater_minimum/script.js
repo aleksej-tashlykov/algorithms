c5fbef8534622df610dbd8763bad84a115936c7f
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-09-01 05:28:11 +1000

    Исправленный исходный код

--- 32_elements_greater_minimum/script.js
+++ 32_elements_greater_minimum/script.js
@@ -5,15 +5,17 @@ if (arr.length === 0) {
 }
 
 let min = arr[0];
-for (let i = 0; i < arr.length; i++) {
+
+for (let i = 1; i < arr.length; i++) {
 	if (arr[i] < min) {
 		min = arr[i];
 	}
 }
 
 let count = 0;
-for (let k = 0; k < arr.length; k++) {
-	if (arr[k] === min + 5) {
+
+for (let i = 0; i < arr.length; i++) {
+	if (arr[i] === min + 5) {
 		count++;
 	}
 }
