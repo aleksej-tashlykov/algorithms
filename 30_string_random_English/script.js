0fafcc32d2695a9c3521f92c2923d77afbbb6f5d
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-09-01 05:22:41 +1000

    Исправленный исходный код

--- 30_string_random_English/script.js
+++ 30_string_random_English/script.js
@@ -1,8 +1,10 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 let str = '';
+let index = 0;
+let char = '';
 
 for (let i = 0; i < 100; i++) {
-	const index = Math.floor(Math.random() * alphabet.length);
-	str += alphabet[index];
+	index = Math.floor(Math.random() * 26) + 97;
+	char = String.fromCharCode(index);
+	str += char;
 }
 console.log(str);
