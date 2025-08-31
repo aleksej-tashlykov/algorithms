c29b55ffda982eb7cbe55d9e69a27f68c9a9d254
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-09-01 05:25:15 +1000

    Исправленный исходный код

--- 31_find_number_words/script.js
+++ 31_find_number_words/script.js
@@ -1,12 +1,9 @@
-const str = '  hello  s   world  from   JavaScript  ';
+const str = '  hello s   world  from   JavaScript  ';
 let count = 0;
 let inWord = false;
 
 for (let i = 0; i < str.length; i++) {
-	const char = str[i];
-	const letters = (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z');
-
-	if (letters) {
+	if (str[i] !== ' ') {
 		if (!inWord) {
 			count++;
 			inWord = true;
