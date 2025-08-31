7d683f754f3b9d637bb45e77e52b4e46dd871b5a
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-09-01 05:12:58 +1000

    Исправленный исходный код

--- 26_maximum_length_consecutive_elements/script.js
+++ 26_maximum_length_consecutive_elements/script.js
@@ -1,3 +1,5 @@
+const arr = [0, 1, 3, 6, 7, 8, 2, 3];
+
 if (arr.length === 0) {
 	return;
 }
@@ -12,12 +14,16 @@ let maxLength = 1;
 for (let i = 0; i < arr.length - 1; i++) {
 	if (arr[i + 1] === arr[i] + 1) {
 		currentLength++;
+	} else {
 		if (currentLength > maxLength) {
 			maxLength = currentLength;
 		}
-	} else {
 		currentLength = 1;
 	}
+}
+
+if (currentLength > maxLength) {
+	maxLength = currentLength;
 }
 
 console.log(maxLength);
