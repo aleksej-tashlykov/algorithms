8017e5378bb2d582ee3484b715ebefdca16430e9
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-09-07 13:45:41 +1000

    Исправленный исходный код

--- 20_shifting_elements/script.js
+++ 20_shifting_elements/script.js
@@ -1,31 +1,19 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const offset = 2;
-
-if (arr.length === 0) {
-	return;
-}
-
-const length = arr.length;
-
-let startIndex, endIndex;
-
-if (offset > 0) {
-	startIndex = 0;
-	endIndex = offset;
-
-	for (let i = length - 1 - offset; i >= 0; i--) {
-		arr[i + offset] = arr[i];
+function shiftArray(arr, offset) {
+	if (arr.length === 0) {
+		return;
 	}
-} else if (offset < 0) {
-	const shift = -offset;
-	startIndex = length - shift;
-	endIndex = length;
 
-	for (let i = 0; i < length - shift; i++) {
-		arr[i] = arr[i + shift];
+	let result = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		result[i] = 0;
 	}
-}
 
-for (let i = startIndex; i < endIndex; i++) {
-	arr[i] = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (i + offset >= 0 && i + offset < arr.length) {
+			result[i + offset] = arr[i];
+		}
+	}
+
+	return result;
 }
