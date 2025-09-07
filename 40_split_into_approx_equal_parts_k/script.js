085fe4ca875a074948c84797a2b5157b8b197d50
aleksej-tashlykov <aleksej.tashlykov@gmail.com>
2025-09-07 13:04:23 +1000

    Исправленный исходный код

--- 40_split_into_approx_equal_parts_k/script.js
+++ 40_split_into_approx_equal_parts_k/script.js
@@ -1,21 +1,21 @@
-let n = 12;
-let k = 5;
+let arrayLength = 12;
+let numberOfSegments = 5;
 
 const numbers = [];
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < arrayLength; i++) {
 	numbers[i] = i + 1;
 }
 
 const parts = [];
 
-const baseSize = Math.floor(n / k);
-const extraParts = n % k;
+const baseSize = Math.floor(arrayLength / numberOfSegments);
+const extraParts = arrayLength % numberOfSegments;
 
 let startIndex = 0;
 let currentPartSize = 0;
 let part = [];
 
-for (let i = 0; i < k; i++) {
+for (let i = 0; i < numberOfSegments; i++) {
 	currentPartSize = baseSize;
 	if (i < extraParts) {
 		currentPartSize = currentPartSize + 1;
@@ -24,7 +24,7 @@ for (let i = 0; i < k; i++) {
 	part = [];
 
 	for (let j = 0; j < currentPartSize; j++) {
-		if (startIndex + j < n) {
+		if (startIndex + j < arrayLength) {
 			part[j] = numbers[startIndex + j];
 		}
 	}
